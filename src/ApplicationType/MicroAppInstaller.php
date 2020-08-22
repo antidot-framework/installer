c37df7898d2ccd4fd215ca70d1d4857c46157c1f
kpicaza <kpicaza.sound@gmail.com>
2020-08-22 21:20:25 +0200

    issue-2: add select namespace question support

--- src/ApplicationType/MicroAppInstaller.php
+++ src/ApplicationType/MicroAppInstaller.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace Antidot\Installer\ApplicationType;
 
 use Antidot\Installer\Question\InstallationPath;
+use Antidot\Installer\Template\ComposerJson;
 use Antidot\Installer\Template\Micro\FileStructure;
 use Composer\Composer;
-use Composer\Factory;
 use Composer\IO\IOInterface;
-use Composer\Json\JsonFile;
-use Composer\Json\JsonManipulator;
 
 use function dirname;
 use function exec;
@@ -20,7 +18,7 @@ use function unlink;
 class MicroAppInstaller implements App
 {
     public const DEPENDENCIES = [
-        'antidot-fw/framework' => 'dev-master',//'^0.1.2',
+        'antidot-fw/framework' => '^0.1.2',
         'antidot-fw/container' => '^0.1.1',
         'antidot-fw/fast-router-adapter' => '^0.1.0',
     ];
@@ -47,32 +45,13 @@ class MicroAppInstaller implements App
             @unlink($installationPath . $fileToDelete);
         }
 
-        // Update composer.json (project is proprietary by default)
-        /** @psalm-suppress MixedArgument */
-        $json = new JsonFile(Factory::getComposerFile());
-        $contents = file_get_contents($json->getPath());
-        $manipulator = new JsonManipulator($contents);
-
-        // new projects are most of the time proprietary
-        $manipulator->addMainKey('license', 'proprietary');
-
-        foreach (self::DEPENDENCIES as $package => $version) {
-            $manipulator->addLink('require', $package, $version);
-        }
-
-        /** @psalm-suppress MixedArgument */
-        $contents = preg_replace(
-            [
-                '{^\s*+"name":.*,$\n}m',
-                '{^\s*+"description":.*,$\n}m',
-                '{^\s*+"antidot-fw\/installer":.*,$\n}m',
-                '{^\s*+"repositories":.*$\n^\s*+\{$\n^\s*+.*,$\n^\s*+.*$\n^\s*+\}$\n^\s*+\],$\n}m'
-            ],
-            '',
-            $manipulator->getContents(),
-            1
-        );
-        file_put_contents($installationPath . '/composer.json', $contents);
+        $manipulator = new ComposerJson($io);
+        $manipulator->prepare($installationPath, self::DEPENDENCIES, [
+            '{^\s*+"name":.*,$\n}m',
+            '{^\s*+"description":.*,$\n}m',
+            '{^\s*+"antidot-fw\/installer":.*,$\n}m',
+            '{^\s*+"repositories":.*$\n^\s*+\{$\n^\s*+.*,$\n^\s*+.*$\n^\s*+\}$\n^\s*+\],$\n}m' // only for development
+        ]);
 
         exec(sprintf('cd %s && rm -rf vendor/ composer.lock && composer install  --ansi', $installationPath));
     }
